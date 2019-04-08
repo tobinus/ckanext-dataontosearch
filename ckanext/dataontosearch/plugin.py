50a664aeee0960a27377a07efac6ca9be9b625e0
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2019-04-08 15:21:43 +0200

    Create skeleton for new action for listing concepts

--- ckanext/dataontosearch/plugin.py
+++ ckanext/dataontosearch/plugin.py
@@ -1,4 +1,3 @@
-from ckan.common import config
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
@@ -8,6 +7,8 @@ class DataOntoSearch_TaggingPlugin(plugins.SingletonPlugin):
     Plugin for tagging datasets with relevant concepts.
     """
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IActions)
+    plugins.implements(plugins.IAuthFunctions)
 
     # IConfigurer
 
@@ -15,6 +16,42 @@ class DataOntoSearch_TaggingPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'dataontosearch')
+
+    # IActions
+
+    def get_actions(self):
+        return {
+            'dataontosearch_concept_list': dataontosearch_concept_list,
+        }
+
+    # IAuthFunctions
+
+    def get_auth_functions(self):
+        return {
+            'dataontosearch_concept_list': dataontosearch_concept_list_auth,
+        }
+
+
+@toolkit.side_effect_free
+def dataontosearch_concept_list(context, data_dict):
+    '''
+    List concepts available from DataOntoSearch.
+
+    :rtype: list of strings
+    '''
+    toolkit.check_access('dataontosearch_concept_list', context, data_dict)
+    # TODO: Make actual request, instead of using example data
+    return [
+        {'uri': 'http://example.com/#more-testing', 'label': 'more testing'},
+        {'uri': 'http://example.com/#yet-an-example', 'label': 'Yet another example'},
+    ]
+
+
+def dataontosearch_concept_list_auth(context, data_dict):
+    # Simply require users to be logged in
+    return {
+        'success': True
+    }
 
 
 class DataOntoSearch_SearchingPlugin(plugins.SingletonPlugin):
@@ -32,16 +69,17 @@ class DataOntoSearch_SearchingPlugin(plugins.SingletonPlugin):
 
 
 def get_tagger_url():
-    tagger_url = config['ckan.dataontosearch.tagger_url']
+    tagger_url = toolkit.config['ckan.dataontosearch.tagger_url']
     return tagger_url
 
 
 def get_search_url():
-    search_url = config['ckan.dataontosearch.search_url']
+    search_url = toolkit.config['ckan.dataontosearch.search_url']
     return search_url
 
 
 def get_credentials():
+    config = toolkit.config
     username = config.get('ckan.dataontosearch.username')
     password = config.get('ckan.dataontosearch.password')
 
@@ -49,5 +87,5 @@ def get_credentials():
 
 
 def get_configuration():
-    configuration = config.get('ckan.dataontosearch.configuration')
+    configuration = toolkit.config.get('ckan.dataontosearch.configuration')
     return configuration
