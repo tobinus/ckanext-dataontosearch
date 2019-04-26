43635a42c20a764fb317964435ee6480184f7d88
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2019-04-26 15:08:31 +0200

    Implement search route

--- ckanext/dataontosearch/views/search.py
+++ ckanext/dataontosearch/views/search.py
@@ -1,7 +1,12 @@
 # encoding: utf-8
 import logging
+import ckan.plugins.toolkit as toolkit
 
-from flask import Blueprint
+from flask import Blueprint, request
+
+from ckanext.dataontosearch.views.utils import _log_exceptions
+
+logger = logging.getLogger(__name__)
 
 search = Blueprint(
     u'dataontosearch_search',
@@ -9,4 +14,26 @@ search = Blueprint(
     url_prefix=u'/dataontosearch/search'
 )
 
-# TODO: Add route for performing and showing search queries
+
+@search.route(u'/')
+@_log_exceptions
+def do_search():
+    context = {}
+
+    query = request.args.get(u'q', u'').strip()
+
+    if query:
+        result = toolkit.get_action(u'dataontosearch_dataset_search')(
+            context,
+            {u'q': query}
+        )
+    else:
+        result = None
+
+    return toolkit.render(
+        u'dataontosearch_search.html',
+        {
+            u'search': result,
+            u'query': query,
+        }
+    )
